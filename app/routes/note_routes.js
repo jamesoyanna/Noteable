dceefb93dbeb257650bb0c4c323a50d9cc312c59
eshoiza <engrjayt200@gmail.com>
2020-09-26 13:55:22 +0100

    Add mongodb client

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,5 +1,10 @@
 module.exports = function(app ,db){
 app.post('/notes', (req, res)=>{
+const note = {text: req.body.body, tile: req.body.title} 
+db.collection('notes').insert(note, (err, results) =>{
+
+})
+
     console.log(req.body)
     res.send('Hello my people')
 })
